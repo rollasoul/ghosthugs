a8e2410d701edbc4445166f0bd5b446cecfef31e
Roland Arnoldt <roland@ouiouioui.space>
2017-03-08 01:33:52 +0200

    Create 8266_hugs.py

---
+++ 8266_server/8266_hugs.py
@@ -0,0 +1,24 @@
+import socket
+import time
+import machine  
+s = socket.socket()
+host = "147.75.197.237"
+port = 12344
+while True:
+    try:
+        while True:
+            s = socket.socket()
+            s.connect((host, port))
+            time.sleep(1)
+            s.send("ready")
+            output = s.recv(2048)
+            if "hug" in output:
+                p12 = machine.Pin(12)
+                pwm12 = machine.PWM(p12)
+                servo = machine.PWM(machine.Pin(12), freq=50)
+                servo.duty(44)
+                time.sleep(1)
+                servo.duty(115)
+            s.close()
+    except:
+        pass
